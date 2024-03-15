ecc4acab550fa9bd2e6f2795b6ccbabbe06b4bdc
azamman <>
2024-03-15 17:41:26 +0900

    3/15 study

--- 2024031501.sql
+++ 2024031501.sql
@@ -16,7 +16,23 @@ WHERE EXTRACT(MONTH FROM SYSDATE) = EXTRACT(MONTH FROM MEM_BIR)
      AND EXTRACT(DAY FROM MEM_BIR) BETWEEN EXTRACT(DAY FROM SYSDATE)
                                                                           AND EXTRACT(DAY FROM SYSDATE+7);
     
- 2. ADD_MONTHS() 
+ 2. ADD_MONTHS(d1,n) 
+    - 주어진 날짜자료 d1에  n개월을 더한 날짜 반환
+사용 예시)
+HR 계정의 사원테이블에서 입사일(HIRE_DATE)을 15년을 더한 날짜로 변경하시오
+UPDATE HR.EMPLOYEES
+SET HIRE_DATE=ADD_MONTHS(HIRE_DATE,180);
+
+SELECT EMP_NAME,HIRE_DATE
+FROM HR.EMPLOYEES;
+COMMIT;
+
+** UPDATE 문의 사용 형식
+ UPDATE 테이블명
+ SET 컬럼명=값[,]
+                :
+        컬럼명=값
+[WHERE 조건]
  
 - NEXT_DAT, LAST_DAY : (주어진 날짜 이후 가장 빠른 요일을 반환, 가장 마지막날을 반환)
 - MONTHS_BETWEEN() : (두 날짜자료 사이의 달수를 반환)
